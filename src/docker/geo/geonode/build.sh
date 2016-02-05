9610e2b48a4fd7ecfcabf74a9ce66d4f1337c9a7
Dave Morris <dmr@roe.ac.uk>
2016-02-05 18:46:52 +0000

    Install notes

---
+++ src/docker/geo/geonode/build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# <meta:header>
+#   <meta:licence>
+#     Copyright (c) 2016, ROE (http://www.roe.ac.uk/)
+#
+#     This information is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This information is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#  
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   </meta:licence>
+# </meta:header>
+#
+#
+
+# -----------------------------------------------------
+# Install our build tools.
+#[root@builder]
+
+    apt-get update
+    apt-get -y install         \
+        build-essential        \
+        patch                  \
+        gcc                    \
+        git                    \
+
+# -----------------------------------------------------
+# Install our dependencies.
+#[root@builder]
+
+    apt-get update
+    apt-get -y install         \
+        python                 \
+        python-dev             \
+        python-gdal            \
+        python-pycurl          \
+        python-imaging         \
+        python-pastescript     \
+        python-psycopg2        \
+        python-support         \
+        python-urlgrabber      \
+        python-virtualenv      \
+
+    apt-get -y install         \
+        libxml2-dev            \
+        libxslt-dev            \
+
+    apt-get -y install         \
+        gettext                \
+
+    apt-get -y install         \
+        libjpeg-dev            \
+        libpng-dev             \
+        libpq-dev              \
+
+    apt-get -y install         \
+        libgeos-dev            \
+        libproj-dev            \
+        gdal-bin               \
+
+# -----------------------------------------------------
+# Clone the GeoNode source code.
+#[root@builder]
+
+    git clone https://github.com/GeoNode/geonode.git /goenode
+
+# -----------------------------------------------------
+# Install GeoNode.
+#[root@builder]
+    
+    pip install -e /geonode
+
+
+
---
+++ src/qgis/styles/faded-green.qml
@@ -0,0 +1,18 @@
+<!DOCTYPE qgis PUBLIC 'http://mrcc.com/qgis.dtd' 'SYSTEM'>
+<qgis version="2.10.1-Pisa" minimumScale="-4.65661e-10" maximumScale="1e+08" hasScaleBasedVisibilityFlag="0">
+  <pipe>
+    <rasterrenderer opacity="1" alphaBand="0" classificationMax="18789.3" classificationMinMaxOrigin="CumulativeCutFullExtentEstimated" band="1" classificationMin="939.467" type="singlebandpseudocolor">
+      <rasterTransparency/>
+      <rastershader>
+        <colorrampshader colorRampType="INTERPOLATED" clip="0">
+          <item alpha="58" value="939.467" label="939.467000" color="#fff583"/>
+          <item alpha="181" value="18789.3" label="18789.300000" color="#1da508"/>
+        </colorrampshader>
+      </rastershader>
+    </rasterrenderer>
+    <brightnesscontrast brightness="0" contrast="0"/>
+    <huesaturation colorizeGreen="128" colorizeOn="0" colorizeRed="255" colorizeBlue="128" grayscaleMode="0" saturation="0" colorizeStrength="100"/>
+    <rasterresampler maxOversampling="2"/>
+  </pipe>
+  <blendMode>0</blendMode>
+</qgis>
